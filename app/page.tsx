804d5a60d1b8a3e8080d373e273f883f2b4ee1d5
YeonHoit <pdbx12@gmail.com>
2023-09-20 19:39:32 +0900

    공통 컴포넌트(Card, Paging)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import {
   Card,
   CardBody,
+  CardBodyTop,
   CardContent,
   CardImage,
+  CardMoreButton,
   CardTag,
+  CardTagWrap,
+  CardText,
   CardTitle,
 } from "@/components/card/Card";
+import {
+  Paging,
+  PagingFirstButton,
+  PagingLastButton,
+  PagingNextButton,
+  PagingNumberButton,
+  PagingNumberText,
+  PagingNumberWrap,
+  PagingPrevButton,
+} from "@/components/paging/Paging";
+import React from "react";
 
 const tags = [
   { tagName: "react", tagId: 1 },
@@ -23,47 +38,59 @@ export default function Home() {
       <Card>
         <CardImage />
         <CardBody>
-          <CardTag>
-            <ul
-              style={{
-                display: "flex",
-                gap: "18px",
-              }}
-            >
-              {tags.map((tag: any) => (
-                <li
-                  key={tag.tagId}
-                  style={{
-                    color: "var(--mint, #00C7AF)",
-                    fontFamily: "Godo M",
-                    fontSize: "18px",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                    lineHeight: "normal",
-                  }}
-                >
-                  #{tag.tagName}
-                </li>
-              ))}
-            </ul>
-            <button>+ more</button>
-          </CardTag>
-          <CardTitle>안녕하세요 만나서 반갑니다.</CardTitle>
-          <CardContent style={{ width: "1040px" }}>
-            여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이
-            4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄 보여지는
-            영역입니다.여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는
-            게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄
-            보여지는 영역입니다.여기는 게시글 내용이 4줄 보여지는
-            영역입니다.여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는
-            게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄
-            보여지는 영역입니다.여기는 게시글 내용이 4줄 보여지는
-            영역입니다.여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는
-            게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄
-            보여지는 영역입니다.
+          <CardBodyTop>
+            <CardTagWrap>
+              {tags.map(
+                (item: { tagName: string; tagId: number }, index: number) => {
+                  return <CardTag key={index}>#{item.tagName}</CardTag>;
+                }
+              )}
+            </CardTagWrap>
+            <CardMoreButton />
+          </CardBodyTop>
+          <CardContent>
+            <CardTitle>여기는 타이틀영역 입니다.</CardTitle>
+            <CardText>
+              여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이
+              4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄 보여지는
+              영역입니다.여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는
+              게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄
+              보여지는 영역입니다.여기는 게시글 내용이 4줄 보여지는
+              영역입니다.여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는
+              게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄
+              보여지는 영역입니다.여기는 게시글 내용이 4줄 보여지는
+              영역입니다.여기는 게시글 내용이 4줄 보여지는 영역입니다.여기는
+              게시글 내용이 4줄 보여지는 영역입니다.여기는 게시글 내용이 4줄
+              보여지는 영역입니다.
+            </CardText>
           </CardContent>
         </CardBody>
       </Card>
+      {/* 구분선 */}
+      <span style={{ height: "30px", width: "100%", display: "block" }}></span>
+      <Paging>
+        <PagingFirstButton />
+        <PagingPrevButton />
+        <PagingNumberWrap>
+          <PagingNumberButton active={true}>
+            <PagingNumberText>1</PagingNumberText>
+          </PagingNumberButton>
+          <PagingNumberButton>
+            <PagingNumberText>2</PagingNumberText>
+          </PagingNumberButton>
+          <PagingNumberButton>
+            <PagingNumberText>3</PagingNumberText>
+          </PagingNumberButton>
+          <PagingNumberButton>
+            <PagingNumberText>4</PagingNumberText>
+          </PagingNumberButton>
+          <PagingNumberButton>
+            <PagingNumberText>5</PagingNumberText>
+          </PagingNumberButton>
+          <PagingNextButton />
+          <PagingLastButton />
+        </PagingNumberWrap>
+      </Paging>
     </div>
   );
 }
